6f9577472d9486a0c74d7431d28b481edf0e3e24
yasamansharei <yasamansharei@gmail.com>
2025-10-14 22:20:02 -0700

    Final

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import data
 import helpers
 import time
 from pages import UrbanRoutesPage
-BASE_URL = "https://cnt-35c86278-39fe-43c5-91d3-d3c5c5522c39.containerhub.tripleten-services.com/"
 
 class TestUrbanRoutes:
     @classmethod
@@ -24,7 +23,7 @@ class TestUrbanRoutes:
 
     def test_set_route(self):
         # Add in S8: Implement the steps for setting a route
-        self.driver.get(BASE_URL)
+        self.driver.get(data.URBAN_ROUTES_URL)
         urban_routes_page = UrbanRoutesPage(self.driver)
         urban_routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
         from_value = urban_routes_page.get_from_location()
@@ -33,28 +32,23 @@ class TestUrbanRoutes:
         expected_value_to = "1300 1st St"
         assert from_value == expected_value_from
         assert to_value == expected_value_to
-        print("function created for set_route")
-        pass
+
 
     def test_select_plan(self):
         # Add in S8: Implement the steps for selecting a plan
-        self.driver.get(BASE_URL)
+        self.driver.get(data.URBAN_ROUTES_URL)
         urban_routes_page = UrbanRoutesPage(self.driver)
         urban_routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
-        time.sleep(2)
         urban_routes_page.click_call_a_taxi()
-        time.sleep(2)
         urban_routes_page.click_supportive()
-        time.sleep(2)
         actual_value = urban_routes_page.get_supportive_option()
         expected_value = "Supportive"
         assert expected_value in actual_value, f"Expected '{expected_value}', but got '{actual_value}'"
-        print("function created for select plan")
-        pass
+
 
     def test_fill_phone_number(self):
         # Add in S8: Implement the steps for filling in a phone number
-        self.driver.get(BASE_URL)
+        self.driver.get(data.URBAN_ROUTES_URL)
         urban_routes_page = UrbanRoutesPage(self.driver)
         urban_routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
         urban_routes_page.click_call_a_taxi()
@@ -66,12 +60,11 @@ class TestUrbanRoutes:
         urban_routes_page.click_confirm_phone()
         actual_phone = urban_routes_page.get_phone_number()
         assert data.PHONE_NUMBER == actual_phone, f"Expected '{data.PHONE_NUMBER}', got '{actual_phone}'"
-    print("Function created for fill_phone_number")
 
 
     def test_fill_card(self):
         # Add in S8: Implement the steps for filling in a card
-        self.driver.get(BASE_URL)
+        self.driver.get(data.URBAN_ROUTES_URL)
         urban_routes_page = UrbanRoutesPage(self.driver)
         urban_routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
         urban_routes_page.click_call_a_taxi()
@@ -79,12 +72,11 @@ class TestUrbanRoutes:
         urban_routes_page.fill_payment_method(data.CARD_NUMBER, data.CARD_CODE)
         actual_value = urban_routes_page.get_payment_method()
         assert "Card" in actual_value, f"Expected 'Card', got '{actual_value}'"
-        print("function created for fill card")
-        pass
+
 
     def test_comment_for_driver(self):
         # Add in S8: Implement the steps for adding a comment for the driver
-        self.driver.get(BASE_URL)
+        self.driver.get(data.URBAN_ROUTES_URL)
         urban_routes_page = UrbanRoutesPage(self.driver)
         urban_routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
         urban_routes_page.click_call_a_taxi()
@@ -92,24 +84,21 @@ class TestUrbanRoutes:
         urban_routes_page.enter_comment(data.MESSAGE_FOR_DRIVER)
         actual_comment = urban_routes_page.get_comment_text()
         assert data.MESSAGE_FOR_DRIVER == actual_comment, f"Expected '{data.MESSAGE_FOR_DRIVER}', got '{actual_comment}'"
-        print("function created for comment for driver")
-        pass
+
 
     def test_order_blanket_and_handkerchiefs(self):
         # Add in S8: Implement the steps for ordering blanket and handkerchiefs
-        self.driver.get(BASE_URL)
+        self.driver.get(data.URBAN_ROUTES_URL)
         urban_routes_page = UrbanRoutesPage(self.driver)
         urban_routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
         urban_routes_page.click_call_a_taxi()
         urban_routes_page.click_supportive()
         urban_routes_page.choose_blanket_and_handkerchiefs()
         assert urban_routes_page.is_blanket_selected()
-        print("function created for order blanket and handkerchiefs")
-        pass
 
     def test_order_2_ice_creams(self):
         # Loop twice since the requirement is to order 2 ice creams
-        self.driver.get(BASE_URL)
+        self.driver.get(data.URBAN_ROUTES_URL)
         urban_routes_page = UrbanRoutesPage(self.driver)
         urban_routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
         urban_routes_page.click_call_a_taxi()
@@ -120,20 +109,18 @@ class TestUrbanRoutes:
             time.sleep(0.3)
 
         assert int(urban_routes_page.check_icecream()) == 2
-        print("function created for order 2 ice creams")
-        pass
+
 
     def test_car_search_model_appears(self):
         # Add in S8: Implement the steps for verifying car search model appears
-        self.driver.get(BASE_URL)
+        self.driver.get(data.URBAN_ROUTES_URL)
         urban_routes_page = UrbanRoutesPage(self.driver)
         urban_routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
         urban_routes_page.click_call_a_taxi()
         urban_routes_page.click_supportive()
         urban_routes_page.enter_comment(data.MESSAGE_FOR_DRIVER)
         urban_routes_page.is_car_search_shown()
-        print("function created for car search model")
-        pass
+
 
 
 
